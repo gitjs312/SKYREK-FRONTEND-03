88d11dbc907f920f1c82dc5fe1234231626a52f0
gitjs321 <githubpractice181@gmail.com>
2026-03-19 13:29:58 +0530

    day-03-completed 😊

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello janith!");
+import express from 'express';
+
+let app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res)=> {
+    console.log('Get request received');
+});
+
+app.post('/', (req, resreq, res) => {   
+    console.log('Post request received');  
+    console.log(req.body);
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000...");
+});
